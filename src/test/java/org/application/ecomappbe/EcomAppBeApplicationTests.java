418a54980925e5621dee332394ecf020a63dc04b
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-03-23 11:31:49 +0300

    initial commit

---
+++ src/test/java/org/application/ecomappbe/EcomAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.application.ecomappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
