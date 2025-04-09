47e2146fbb17cf175682189e4b81eaf5901e8a2e
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-04-09 15:17:43 +0300

    UserRepository created

---
+++ src/main/java/org/application/ecomappbe/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.application.ecomappbe.repository;
+
+import org.application.ecomappbe.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
