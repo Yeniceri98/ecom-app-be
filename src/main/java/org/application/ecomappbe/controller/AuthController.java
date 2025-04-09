ac43b06480c6f2634e8a93de31087e59b5453e45
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-04-09 15:19:24 +0300

    AuthController class implemented

---
+++ src/main/java/org/application/ecomappbe/controller/AuthController.java
@@ -0,0 +1,47 @@
+package org.application.ecomappbe.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.application.ecomappbe.dto.LoginRequest;
+import org.application.ecomappbe.dto.LoginResponse;
+import org.application.ecomappbe.security.jwt.JwtUtils;
+import org.application.ecomappbe.security.user.EcomUserDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    @PutMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwtToken = jwtUtils.generateToken(authentication);
+
+        EcomUserDetails userDetails = (EcomUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return ResponseEntity.ok(new LoginResponse(userDetails.getUsername(), jwtToken, roles));
+    }
+}
