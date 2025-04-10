15093e71cbf6e2ce415f97d0649203ef7f8b3646
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-04-10 11:55:25 +0300

    Additional requestMatchers implemented

--- src/main/java/org/application/ecomappbe/security/SecurityConfig.java
+++ src/main/java/org/application/ecomappbe/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**",
-                                "api/admin/**",
+                                "/api/admin/**",
                                 "/h2-console/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
