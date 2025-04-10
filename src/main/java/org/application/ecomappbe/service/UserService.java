621c687efafc0357ca0aca36cf7ef37dfbe9a860
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-04-10 14:19:19 +0300

    fix in interface

--- src/main/java/org/application/ecomappbe/service/UserService.java
+++ src/main/java/org/application/ecomappbe/service/UserService.java
@@ -1,9 +1,8 @@
 package org.application.ecomappbe.service;
 
 import org.application.ecomappbe.dto.RegisterRequest;
-import org.application.ecomappbe.exception.UserAlreadyExistsException;
 import org.application.ecomappbe.model.User;
 
 public interface UserService {
-    User register(RegisterRequest registerRequest) throws UserAlreadyExistsException;
+    User register(RegisterRequest registerRequest);
 }
