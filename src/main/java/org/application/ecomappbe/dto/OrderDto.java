f446a127e334c60a13d13f5087505c9513992da2
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-05-05 14:34:37 +0300

    OrderDto is updated by adding JsonFormat & Fixing orderItemsDto initialization

--- src/main/java/org/application/ecomappbe/dto/OrderDto.java
+++ src/main/java/org/application/ecomappbe/dto/OrderDto.java
@@ -1,11 +1,13 @@
 package org.application.ecomappbe.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -17,10 +19,12 @@ public class OrderDto {
     @Email
     private String orderEmail;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate orderDate;
+
     private Double totalAmount;
     private String orderStatus;
-    private List<OrderItemDto> orderItemsDto;
+    private List<OrderItemDto> orderItemsDto = new ArrayList<>();
     private PaymentDto paymentDto;
     private Long addressId;
 }
